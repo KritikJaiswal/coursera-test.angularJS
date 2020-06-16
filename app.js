87818a9b1c42f887eab8b8d801243741a69aa356
kritik <kritikjaiswal11@gmail.com>
2020-06-16 20:37:47 +0530

    initial

---
+++ app.js
@@ -0,0 +1,56 @@
+(function () {
+	'use strict';
+
+	angular.module('LunchCheck',[]).controller('LunchCheckController', LunchCheckController);
+
+	LunchCheckController.$inject =['$scope'];
+
+	function LunchCheckController ($scope){
+		$scope.list = '';
+		$scope.msg = '';
+		$scope.fontStyle = {};
+		$scope.boxStyle = {};
+		
+		$scope.checkItems = function () {
+			console.log ('check Items');
+			console.log ($scope.list);
+			if($scope.list !== '') {
+				var arr = $scope.list.split(',');
+				var count =0;
+				for(var i =0; i<arr.length; i++){
+					if(arr[i].trim() !=='')
+						count++;
+				}
+				if(count > 0){
+					if(count <=3){
+						$scope.msg = 'Enjoy!';
+					}
+					else if(count > 3){
+						$scope.msg = 'Too much!'
+					}
+					$scope.fontStyle = {
+						"color":"green"
+					};
+					$scope.boxStyle = {
+						"border-color":"green"
+					};
+				}
+				else {
+					enterItems();
+				}
+			}
+			else {
+				enterItems();
+			}
+		}
+		var enterItems = function() {
+			$scope.msg = 'Please enter data first';
+				$scope.fontStyle = {
+					"color":"red"
+				};
+				$scope.boxStyle = {
+					"border-color":"red"
+				};
+		};
+	}
+})();
